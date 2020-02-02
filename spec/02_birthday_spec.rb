eb368d4e2577f5d3c11baf789ca3397f6d5a9b82
Willis Aybar <willisaybar@gmail.com>
2020-02-02 19:43:42 +0000

    Automatically backed up by Learn

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -28,3 +28,8 @@ describe "#age_appropriate_birthday" do
   end
 end
 
+def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
