667ae3a205498159afacb0d196af535dbf85108d
Willis Aybar <willisaybar@gmail.com>
2020-02-02 19:48:21 +0000

    Automatically backed up by Learn

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -29,9 +29,10 @@ describe "#age_appropriate_birthday" do
 end
 
 def happy_birthday(birthday_kids)
-  birthday_kids= ""
-  if birthday_kids.each do |kids_name, age|
+  greeting= ""
+  if birthday_kids == |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     elsif 
   end
+  greeting
 end
